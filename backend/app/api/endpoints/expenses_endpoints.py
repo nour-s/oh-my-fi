1d3f87b242f1d2977c9d3a29c0d8a2a4458f996a
Nour Sabouny <>
2024-08-11 15:11:01 +0400

    fix dependency injection for database

--- backend/app/api/endpoints/expenses_endpoints.py
+++ backend/app/api/endpoints/expenses_endpoints.py
@@ -1,25 +1,28 @@
 from fastapi import APIRouter, Depends
 from app.services.expense_service import ExpenseService
 from app.api.schemas.expense_schema import ExpenseCreate, ExpenseResponse
-from infra.dependency_injection.container import AppContainer as container
+from infra.dependency_injection.container import AppContainer
+from dependency_injector.wiring import Provide, inject
+from infra.dependency_injection.container import AppContainer
 
 router = APIRouter()
 
-def get_expense_service() -> ExpenseService:
-    return container.expense_service()
-
 @router.post("/expenses/", response_model=ExpenseResponse)
-def create_expense(expense: ExpenseCreate, expense_service: ExpenseService = Depends(get_expense_service)):
+@inject
+def create_expense(expense: ExpenseCreate, expense_service: ExpenseService = Depends(Provide[AppContainer.expense_service])):
     return expense_service.create_expense(expense)
 
 @router.get("/expenses/{expense_id}", response_model=ExpenseResponse)
-def get_expense(expense_id: int, expense_service: ExpenseService = Depends(get_expense_service)):
+@inject
+def get_expense(expense_id: int, expense_service: ExpenseService = Depends(Provide[AppContainer.expense_service])):
     return expense_service.get_expense_by_id(expense_id)
 
 @router.put("/expenses/{expense_id}", response_model=ExpenseResponse)
-def update_expense(expense_id: int, expense: ExpenseCreate, expense_service: ExpenseService = Depends(get_expense_service)):
+@inject
+def update_expense(expense_id: int, expense: ExpenseCreate, expense_service: ExpenseService = Depends(Provide[AppContainer.expense_service])):
     return expense_service.update_expense(expense_id, expense)
 
 @router.delete("/expenses/{expense_id}")
-def delete_expense(expense_id: int, expense_service: ExpenseService = Depends(get_expense_service)):
+@inject
+def delete_expense(expense_id: int, expense_service: ExpenseService = Depends(Provide[AppContainer.expense_service])):
     return expense_service.delete_expense(expense_id)
